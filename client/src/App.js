086c097d65e6126c854357c2fcd596081a94dc47
Prince Rakholiya <princerakholiya123@example.com>
2023-03-18 12:06:27 +0530

    added

--- client/src/App.js
+++ client/src/App.js
@@ -4,13 +4,17 @@ import {
   Routes,
   Route
 } from "react-router-dom";
-import Home from "./pages/Home";
+import Home from "./pages/home/Home";
+import List from "./pages/list/List";
+import Hotel from "./pages/hotel/Hotel";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home/>} />
+        <Route path="/hotels" element={<List/>} />
+        <Route path="/hotels/:id" element={<Hotel/>} />
       </Routes>
     </BrowserRouter>
   );
